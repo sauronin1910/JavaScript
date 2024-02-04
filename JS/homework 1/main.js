d7b8b6287b81df3c6b0620b54f469b6033e14400
Serhii <serhiisaunin@gmail.com>
2024-02-04 02:12:52 +0200

    home work 1

---
+++ JS/homework 1/main.js
@@ -0,0 +1,15 @@
+let walk = prompt("введіть скільки ви в середньому робите кроків на день");
+
+let walkMonth = prompt("вкажіть кількість днів");
+
+let summarily = walk * walkMonth
+alert("сумарно за цей час ви зробили " + summarily);
+
+
+
+// // Розрахунок оплати за електроенергію
+// var electricityRate = prompt("Введіть ставку за кіловат-годину:");
+// var electricityUsage = prompt("Введіть кількість кіловат-годин спожито за місяць:");
+
+// var electricityBill = electricityRate * electricityUsage;
+// alert("Оплата за електроенергію: $" + electricityBill);
