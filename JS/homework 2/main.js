cd4baf66469c65d894585673bb2dabeb9254d254
Serhii <serhiisaunin@gmail.com>
2024-02-11 01:02:04 +0200

    homework2

---
+++ JS/homework 2/main.js
@@ -0,0 +1,44 @@
+//  1 Завдання
+
+// let a = 5;
+// let b, c;
+
+// b = (a * 5);
+// b = (c = (b / 2));
+
+
+
+// 2 Завдання
+
+// let howOldAY = prompt("Скільки Вам років?");
+
+// let year = prompt("який зараз рік?");
+
+// let dateBirth = year - howOldAY;
+
+// alert("Ви народились y  " + dateBirth + " році");
+
+
+// 3 Завдання
+
+// let tempC = prompt("яка зараз температура в °С? ");
+
+// let tempF = (tempC * 9 / 5) + 32;
+
+// alert("Температура  " + tempF + " °F");
+
+
+
+//  4 Завдання
+
+let a = prompt("перший доданок");
+
+let b = prompt("другий доданок");
+
+let result = Math.floor(a / b);
+
+alert("результата " + result);
+
+
+
+
