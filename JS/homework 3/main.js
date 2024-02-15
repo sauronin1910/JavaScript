7701fb748fd0a3d7e6b2a48ddf757a14e4f37c9c
Serhii <serhiisaunin@gmail.com>
2024-02-15 02:43:15 +0200

    hm3

--- JS/homework 3/main.js
+++ JS/homework 3/main.js
@@ -15,8 +15,52 @@
 
 //  Завдання 3
 
+// {
+//     let str = "cANBerRa";
+//     let result = str[0].toUpperCase() + str.slice(1).toLowerCase();
+//     console.log(result);
+// }
+
+//  Завдання 4
+
+// {
+//     let str = prompt("введіть слова через пробіл")
+
+//     let wordStr = str.split(" ");
+//     let contsStr = wordStr.length;
+//     alert(wordStr + contsStr);
+
+// }
+
+// Завдання 5
+
+// {
+//     const name1 = prompt("напишіть ваше ім'я").trim();
+
+//     const surname1 = prompt("Напишіть ваше прізвище").trim();
+
+//     const surname2 = prompt("по батькові?").trim();
+
+//     let fullName = name1[0].toUpperCase() + name1.slice(1).toLowerCase() + " " +
+//         surname1[0].toUpperCase() + surname1.slice(1).toLowerCase() + " " +
+//         surname2[0].toUpperCase() + surname2.slice(1).toLowerCase() + " ";
+
+//     alert(fullName);
+
+// }
+
+//  Завдання 6
+
 {
-    let str = "cANBerRa";
-    let result = str[0].toUpperCase() + str.slice(1).toLowerCase();
-    console.log(result); 
+    let str = "Було жарко. Василь пив пиво вприкуску з креветками"
+    let result = str.split("пиво").join("чай")
+    console.log(result) //"Було жарко. Василь пив чай уприкуску з креветками"
+
+}
+
+// Завдання 7
+
+{
+
+    
 }
